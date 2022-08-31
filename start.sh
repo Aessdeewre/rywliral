5f0671154fd4fef37b5c0079c5fbd53196bf10a3
Aessdeewre <106429396+Aessdeewre@users.noreply.github.com>
2022-08-31 08:44:23 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # configs
-AUUID=c0b3ef90-3402-11ec-8d3d-0242ac130003
-CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/xray.json
+AUUID=65030eb9-eb9c-479b-a023-bff640e5e6b6
+CADDYIndexPage=https://github.com/Aessdeewre/3Djdwe/raw/main/3DCEList-master.zip
+CONFIGCADDY=https://raw.githubusercontent.com/Aessdeewre/rywliral/master/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/Aessdeewre/rywliral/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/StoreFiles
+StoreFiles=https://raw.githubusercontent.com/Lbingyi/Aessdeewre/rywliral/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
